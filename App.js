c6d58251fdc6149c57558bf89ab6f17f5233a82c
nboisclair64 <nboisclair64@gmail.com>
2021-05-12 16:08:01 -0400

    1

--- App.js
+++ App.js
@@ -1,29 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, Alert, SafeAreaView, TextInput } from 'react-native';
 import { Input } from 'react-native-elements';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 const input = React.createRef();
-var time;
-var numAssign;
 export default function App() {
+  const [time, setTime] = useState('');
+  const [numAssign,setNum] = useState('');
+  const ShowData = () =>
+    Alert.alert(
+      time,
+      numAssign,
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ]
+    );
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
         <Text style={styles.title}>Academic Assistant</Text>
         <Input
-          ref={input}
           placeholder='Time Studying'
+          onChangeText={time => setTime(time)}
           containerStyle={styles.inputBox}
-          onChangeText={value => this.setState({time:value})}
         />
         <Input
           placeholder='Amount of Assignments'
+          onChangeText={numAssign => setNum(numAssign)}
           containerStyle={styles.inputBox}
         />
         <Button style={styles.button}
-        title="Submit"
-      />
+          title="Submit"
+          onPress={ShowData}
+        />
       <StatusBar style="auto" />
       </View>
       </SafeAreaView>
